e9bc1c0f22cd0c254d39e63e68bc9343d743b39b
Milos <milos.murin.mm@gmail.com>
2023-05-23 02:27:41 +0200

    Home service almost done

---
+++ home-service/src/main/java/me/milos/murin/homeservice/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package me.milos.murin.homeservice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "index";
+    }
+
+}
